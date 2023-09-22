a43d79767b8bda1932ce1fe9cffe7c871cc71b96
Ana Savegnagoe <anasavegnago.castro@gmail.com>
2023-09-22 14:40:31 -0300

    css do menu responsivo

--- 2_MENU_RESPONSIVO/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/src/components/Header.jsx
@@ -1,14 +1,24 @@
 import { List } from "@phosphor-icons/react"
 
+import styles from "./Header.module.css"
+
 function Header() {
   return (
     <header>
         <div className="mobile">
-            <div className="menu_logo">
+            <div className={styles.menu_logo}>
                 <button>
                     <List size={32} />
                 </button>
                 <h1>Logo</h1>
+            </div>
+            <div className={styles.opcoes}>
+              <nav>
+                <a href="">Home</a>
+                <a href="">Ferramentas</a>
+                <a href="">Objetivos</a>
+                <a href="">Bibliografia</a>
+              </nav>
             </div>
         </div>
     </header>
