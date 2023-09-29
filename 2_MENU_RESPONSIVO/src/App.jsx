d6fd968b1c5be619716dc3d60e686ce202b0cc0c
Ana Savegnagoe <anasavegnago.castro@gmail.com>
2023-09-29 15:15:22 -0300

    ultimatt

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -1,7 +1,16 @@
 import Header from "./components/Header"
 function App() {
   return (
+    <div>
     <Header />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut reiciendis exercitationem quasi aspernatur at id vitae velit ratione quia asperiores quas, aliquid ipsa! Itaque impedit nobis ab magni laborum.
+      Sit, earum voluptate! Non ad, repellat placeat nulla culpa reprehenderit dolorem laborum quaerat est tenetur dolor explicabo natus reiciendis ipsum voluptate velit, similique, accusantium corporis. Qui incidunt sint odio itaque!
+      Numquam aliquam quasi maiores ea accusamus quo expedita debitis similique, omnis quae voluptatem nihil fuga ducimus amet quaerat? Corrupti iusto incidunt dolor quidem distinctio esse fugit at enim voluptatem consequuntur!
+      Labore, incidunt quaerat nam ut quibusdam quas fugiat animi quisquam? Libero dolorum nostrum est facere perspiciatis accusamus, aut molestiae rem, odit minus fugit impedit, sequi deserunt aspernatur optio consequuntur dolores.
+    </p>
+    </div>
+
   )
 }
 
